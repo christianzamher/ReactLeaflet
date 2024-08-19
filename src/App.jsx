75d861a69bbd822cf73f19a0847406e0a2e13523
wulf <czh1983@gmail.com>
2024-08-19 14:47:53 -0300

    cambios en Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 
 import { Navigate, Route, Routes } from "react-router-dom";
-// import { Landing_Page } from "./Pages/LandingPage/Landing_Page.View";
 import { RoutePlanner } from "./components/RoutePlanner";
 import { Header } from "./components/Header";
 import { Footer } from "./components/UI/Footer";
 import { About } from "./components/About";
 import { AboutRuta } from "./components/AboutRuta";
 
-
 function App() {
   return (
     <>
-      <Header /> 
+      <Header />
       <Routes>
-        <Route path="/" element={<RoutePlanner />}/>
-        <Route path="/equipo" element={<About />}/>
-        <Route path="/ruta" element={<AboutRuta />}/>
-
-        {/* <Route path="/*" element={<Navigate to="/Home" />} /> */}
+        <Route path="/" element={<RoutePlanner />} />
+        <Route path="/equipo" element={<About />} />
+        <Route path="/ruta" element={<AboutRuta />} />
+        <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,37 +4,37 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Link,
+  Link as NextUILink,
   DropdownItem,
   DropdownTrigger,
   Dropdown,
   DropdownMenu,
   Avatar,
 } from "@nextui-org/react";
-//  import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <Navbar className="w-full bg-[#0a1200]/90  z-[9000]">
+    <Navbar className="w-full bg-[#0a1200]/90 z-[9000]">
       <NavbarBrand>
         <p className="font-bold text-inherit text-white">La Ruta del Tango</p>
       </NavbarBrand>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link className="text-white" href="/equipo">
+          <Link className="text-white" to="/equipo">
             Sobre Mí
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
-          <Link href="/ruta" aria-current="page" color="secondary">
+          <Link to="/ruta" aria-current="page" className="text-secondary">
             Customers
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link className="text-white" href="#">
+          <NextUILink className="text-white" href="#">
             Configuracion
-          </Link>
+          </NextUILink>
         </NavbarItem>
       </NavbarContent>
 
@@ -45,42 +45,37 @@ export const Header = () => {
               isBordered
               as="button"
               className="transition-transform bg-orange-600"
-              color="succes"
+              color="success"
               name="Jason Hughes"
               size="sm"
               src="https://imgs.search.brave.com/HZTzT4hztPp3ddCwh8Cty8pS5Q4BLpIv-forUtQrXXw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9mbHlj/bGlwYXJ0LmNvbS90/aHVtYnMvdGFuZ28t/ZGFuY2VyLWNsaXBh/cnQtdHJhbnNwYXJl/bnQtaW1hZ2VzLWJh/bGxyb29tLWRhbmNl/LTE1MzAwNzYucG5n"
+              alt="User Avatar"
             />
           </DropdownTrigger>
           <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem  className="h-14 gap-2 ">
-              <p className=" text-orange-700 ">Bienvenido</p>
+            <DropdownItem className="h-14 gap-2">
+              <p className="text-orange-700">Bienvenido</p>
             </DropdownItem>
-            <DropdownItem key="/">
-              <Link href={"/"}>
-                <p className=" text-black ">Mapa</p>
+            <DropdownItem>
+              <Link to="/">
+                <p className="text-black">Mapa</p>
               </Link>
             </DropdownItem>
             <DropdownItem>
-              <Link href="/equipo">
-                <p className="text-black ">Equipo</p>
+              <Link to="/equipo">
+                <p className="text-black">Equipo</p>
               </Link>
             </DropdownItem>
-            <DropdownItem >
-              <Link>
-                <p className="text-black ">Configuracion</p>
-              </Link>
+            <DropdownItem>
+              <NextUILink href="#">
+                <p className="text-black">Configuracion</p>
+              </NextUILink>
             </DropdownItem>
-            <DropdownItem >
-              <Link href="/ruta" className="text-black ">
+            <DropdownItem>
+              <Link to="/ruta" className="text-black">
                 <p>Acerca de La Ruta</p>
               </Link>
             </DropdownItem>
-         
-         
-
-            {/* <DropdownItem key="logout" color="danger">
-              Log Out
-            </DropdownItem> */}
           </DropdownMenu>
         </Dropdown>
       </NavbarContent>
